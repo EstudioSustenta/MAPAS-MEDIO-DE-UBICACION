12ec9f60f0103594afc4e2fc34c0ea62a54fcbda
Gustavo <esustenta@gmail.com>
2023-11-13 22:54:14 -0600

    prueba de funcionamiento

--- EJECUTABLES/INICIO.py
+++ EJECUTABLES/INICIO.py
@@ -5,7 +5,6 @@
 import sys
 import importlib
 import tkinter as tk
-# import Tkinter as tk
 from tkinter import ttk
 import arcpy
 
@@ -17,88 +16,80 @@ sys.path.append(ruta_libreria)
 
 gensis = importlib.import_module("LIBRERIA.generacion_sistema")
 # reload(gensis)
-# importlib.reload(LIBRERIA.generacion_sistema)
-# importlib.reload(LIBRERIA.MAPAS)
 
 def ini():
 
     print("se selecionó el boton 'nuevo proyecto'")
 
-    # # VENTANA PARA SELECCIONAR EL PROYECTO
+    # VENTANA PARA SELECCIONAR EL PROYECTO
 
-    # # Función para mostrar la selección y cerrar la ventana
-    # def mostrar_seleccion():
-    #     seleccion = entry.get()
-    #     if seleccion:
-    #         print("Selección: " + seleccion)
-    #         ventana.destroy()
-    #         gensis.start(seleccion)
+    # Función para mostrar la selección y cerrar la ventana
+    def mostrar_seleccion():
+        seleccion = entry.get()
+        if seleccion:
+            print("Selección: " + seleccion)
+            ventana.destroy()
+            gensis.start(seleccion)
 
 
-    # # Archivo shapefile de entrada
-    # shapefile = "Y:/0_SIG_PROCESO/ORIGEN/PROYECTOS.shp"
+    # Archivo shapefile de entrada
+    shapefile = "Y:/0_SIG_PROCESO/ORIGEN/PROYECTOS.shp"
 
-    # # Campo 'DESCRIP'
-    # campo_descrip = "DESCRIP"
+    # Campo 'DESCRIP'
+    campo_descrip = "DESCRIP"
 
-    # # Leer todos los valores del campo 'DESCRIP' del shapefile
-    # valores_descrip = [row[0] for row in arcpy.da.SearchCursor(shapefile, [campo_descrip])]
-    # valores_descrip = sorted(set(valores_descrip))
+    # Leer todos los valores del campo 'DESCRIP' del shapefile
+    valores_descrip = [row[0] for row in arcpy.da.SearchCursor(shapefile, [campo_descrip])]
+    valores_descrip = sorted(set(valores_descrip))
 
-    # # Crear una ventana principal
-    # ventana = tk.Tk()
-    # ventana.title("Seleccionar Valor")
+    # Crear una ventana principal
+    ventana = tk.Tk()
+    ventana.title("Seleccionar Valor")
 
-    # # Obtener las dimensiones de la pantalla
-    # ancho_pantalla = ventana.winfo_screenwidth()
-    # alto_pantalla = ventana.winfo_screenheight()
+    # Obtener las dimensiones de la pantalla
+    ancho_pantalla = ventana.winfo_screenwidth()
+    alto_pantalla = ventana.winfo_screenheight()
 
-    # # Calcular el ancho y alto de la ventana
-    # nuevo_ancho = 400  # Cambia este valor según tus preferencias
-    # nuevo_alto = 200  # Cambia este valor según tus preferencias
+    # Calcular el ancho y alto de la ventana
+    nuevo_ancho = 400  # Cambia este valor según tus preferencias
+    nuevo_alto = 200  # Cambia este valor según tus preferencias
 
-    # # Configurar el tamaño y posición de la ventana
-    # x = (ancho_pantalla - nuevo_ancho) // 2
-    # y = (alto_pantalla - nuevo_alto) // 2
-    # ventana.geometry("{}x{}+{}+{}".format(nuevo_ancho, nuevo_alto, x, y)) # ESTA LÍNEA DEFINE DIMENSIONES Y POSICIÓN DE LA VENTANA
+    # Configurar el tamaño y posición de la ventana
+    x = (ancho_pantalla - nuevo_ancho) // 2
+    y = (alto_pantalla - nuevo_alto) // 2
+    ventana.geometry("{}x{}+{}+{}".format(nuevo_ancho, nuevo_alto, x, y)) # ESTA LÍNEA DEFINE DIMENSIONES Y POSICIÓN DE LA VENTANA
 
-    # # Etiqueta con instrucciones
-    # etiqueta = ttk.Label(ventana, text="Selecciona un valor:")
-    # etiqueta.pack(padx=20, pady=10)
+    # Etiqueta con instrucciones
+    etiqueta = ttk.Label(ventana, text="Selecciona un valor:")
+    etiqueta.pack(padx=20, pady=10)
 
-    # # Entry para mostrar los valores y permitir la selección
-    # entry = ttk.Combobox(ventana, values=valores_descrip, width=50)
-    # entry.pack(padx=20)
+    # Entry para mostrar los valores y permitir la selección
+    # global entry  # Declarar entry como global
+    entry = ttk.Combobox(ventana, values=valores_descrip, width=50)
+    entry.pack(padx=20)
 
-    # # Botón para mostrar la selección
-    # yboton = 50   # determina la posición en 'y' del botón
-    # boton_seleccionar = ttk.Button(ventana, text="Seleccionar", command=mostrar_seleccion)
-    # boton_seleccionar.pack(pady=yboton)
+    # Botón para mostrar la selección
+    yboton = 50   # determina la posición en 'y' del botón
+    boton_seleccionar = ttk.Button(ventana, text="Seleccionar", command=mostrar_seleccion)
+    boton_seleccionar.pack(pady=yboton)
 
-    # # Iniciar el bucle principal de la ventana
-    # ventana.mainloop()
+    # Iniciar el bucle principal de la ventana
+    ventana.mainloop()
 
 
 def decision():
 
     # Función que se ejecuta cuando se hace clic en el botón "Sí"
     def nuevo_proyecto():
-        # Aquí puedes poner el código para iniciar tu proceso
-        # ventana.destroy()
-        # arcpy.AddMessage("Proceso iniciado")
+
         print("SELECCIONAR NUEVO PROYECTO")
         ventana1.destroy()
         ini()
 
-        ventSel = importlib.import_module("LIBRERIA.ventana_seleccion")
-        # reload(ventSel)
-        ventSel.ini()
-        importlib.import_module("EJECUTABLES.generacion_sistema")
-
     # Función que se ejecuta cuando se hace clic en el botón "No"
     def proyecto_predefinido():
         ventana1.destroy()
-        arcpy.AddMessage("Proceso no iniciado")
+        # arcpy.AddMessage("Proceso no iniciado")
         print("PROCESO CON EL PROYECTO PREDEFINIDO")
 
     # Crear una ventana1 principal
@@ -144,9 +135,9 @@ def decision():
 
 decision()
 print("\n\nIniciando proceso de generación de mapas")
-# mapas = importlib.import_module("LIBRERIA.MAPAS")
+mapas = importlib.import_module("LIBRERIA.MAPAS")
 # importlib.import_module("LIBRERIA.MAPAS")
-# reload(mapas)
+reload(mapas)
 # mapas.arranque()
 print(u"\n\nFIN DE PROCESO DE GENERACIÓN DE MAPAS")
 #mapas()
